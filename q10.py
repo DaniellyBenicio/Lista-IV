6696a9751ca5d1ee14fa05338a7468c038d58957
DaniellyBenicio <115299113+DaniellyBenicio@users.noreply.github.com>
2023-06-08 23:37:06 -0300

    q10.py

--- q10.py
+++ q10.py
@@ -1,6 +1,4 @@
-
-
-
+'''Implemente uma lista encadeada dupla e as operações básicas: inserir no início, inserir no final, remover do início, remover do final e exibir a lista.'''
 
 class Item:
     def __init__(self, dado):
